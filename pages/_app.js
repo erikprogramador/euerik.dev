d9fd6f2f661126a512dcff503dfb9224871a8fd8
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2021-02-16 02:45:19 -0300

    Adicionando og tags

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,21 @@ export default function MyApp({ Component, pageProps }) {
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap'
           rel='stylesheet'
         />
+
+        {/* Twitter */}
+        <meta name='twitter:card' content='summary' key='twcard' />
+        <meta name='twitter:creator' content='@erikvfernandes' key='twhandle' />
+
+        {/* Open Graph */}
+        <meta property='og:url' content='https://euerik.dev/' key='ogurl' />
+        <meta property='og:image' content='/static/preview.png' key='ogimage' />
+        <meta property='og:site_name' content='EuErik.dev' key='ogsitename' />
+        <meta property='og:title' content='EuErik.dev' key='ogtitle' />
+        <meta
+          property='og:description'
+          content='Desenvolvedor por paixão, Designer por curiosidade, Empreendedor por natureza'
+          key='ogdesc'
+        />
       </Head>
 
       <Component {...pageProps} />
