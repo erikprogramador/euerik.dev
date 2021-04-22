a648bd6fc08b9796a6b4c3d9fde12cf85c9d1576
Erik Vanderlei Fernandes <erik.v.fernandes@gmail.com>
2021-04-21 21:37:58 -0300

    Trocando o icone de abrir e fechar do menu

--- components/Header.js
+++ components/Header.js
@@ -45,12 +45,21 @@ function Header() {
             viewBox='0 0 24 24'
             xmlns='http://www.w3.org/2000/svg'
           >
-            <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
-              d='M4 6h16M4 12h16M4 18h7'
-            ></path>
+            {isMenuOpen ? (
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M6 18L18 6M6 6l12 12'
+              ></path>
+            ) : (
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M4 6h16M4 12h16M4 18h7'
+              ></path>
+            )}
           </svg>
         </button>
 
