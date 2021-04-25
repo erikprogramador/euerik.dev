4b6e7d6774677e1ccbe005bca33216f4818916e9
Erik Vanderlei Fernandes <erik.v.fernandes@gmail.com>
2021-04-25 17:33:16 -0300

    Ajustando svgs

--- components/Header.js
+++ components/Header.js
@@ -47,16 +47,16 @@ function Header() {
           >
             {isMenuOpen ? (
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M6 18L18 6M6 6l12 12'
               ></path>
             ) : (
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M4 6h16M4 12h16M4 18h7'
               ></path>
             )}
